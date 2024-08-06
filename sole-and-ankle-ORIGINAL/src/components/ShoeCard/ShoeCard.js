d890359923c150b3f97cbcd6c89d6410b7b0af66
Eduardo Lange <eduardoandrelange@gmail.com>
2024-08-05 21:22:17 -0300

    finalizando

--- sole-and-ankle-ORIGINAL/src/components/ShoeCard/ShoeCard.js
+++ sole-and-ankle-ORIGINAL/src/components/ShoeCard/ShoeCard.js
@@ -1,36 +1,29 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from 'react'
+import styled from 'styled-components/macro'
 
-import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
-import Spacer from '../Spacer';
+import { COLORS, WEIGHTS } from '../../constants'
+import { formatPrice, pluralize, isNewShoe } from '../../utils'
+import Spacer from '../Spacer'
 
-const ShoeCard = ({
-  slug,
-  name,
-  imageSrc,
-  price,
-  salePrice,
-  releaseDate,
-  numOfColors,
-}) => {
-  // There are 3 variants possible, based on the props:
-  //   - new-release
-  //   - on-sale
-  //   - default
-  //
-  // Any shoe released in the last month will be considered
-  // `new-release`. Any shoe with a `salePrice` will be
-  // on-sale. In theory, it is possible for a shoe to be
-  // both on-sale and new-release, but in this case, `on-sale`
-  // will triumph and be the variant used.
-  // prettier-ignore
-  const variant = typeof salePrice === 'number'
+const ShoeCard = ({ slug, name, imageSrc, price, salePrice, releaseDate, numOfColors }) => {
+	// There are 3 variants possible, based on the props:
+	//   - new-release
+	//   - on-sale
+	//   - default
+	//
+	// Any shoe released in the last month will be considered
+	// `new-release`. Any shoe with a `salePrice` will be
+	// on-sale. In theory, it is possible for a shoe to be
+	// both on-sale and new-release, but in this case, `on-sale`
+	// will triumph and be the variant used.
+	// prettier-ignore
+	const variant = typeof salePrice === 'number'
     ? 'on-sale'
     : isNewShoe(releaseDate)
       ? 'new-release'
       : 'default'
-
+	/* 
+DO MEU JEITINHO KKK
     function returnText(variant) {
       if (variant === 'on-sale') {
         return 'Sale'
@@ -39,85 +32,97 @@ const ShoeCard = ({
       }
     }
 
-  return (
-    <Link href={`/shoe/${slug}`}>
-      <Wrapper>
-        <ImageWrapper>
-          <Flag style={{
-            '--bg-color': variant === 'on-sale' ? COLORS.primary : variant === 'new-release' ? COLORS.secondary : undefined
-          }}>{returnText(variant)}</Flag>
-          <Image alt="" src={imageSrc} />
-        </ImageWrapper>
-        <Spacer size={12} />
-        <Row>
-          <Name>{name}</Name>
-          <Price style={{
-            '--color': variant === 'on-sale' ? COLORS.gray[900] : undefined,
-            '--text-decoration': variant === 'on-sale' ? 'line-through' : undefined   
-          }}>{formatPrice(price)}</Price>
-        </Row>
-        <Row>
-          <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
-          {variant === 'on-sale' ? (
-            <SalePrice>{formatPrice(salePrice)}</SalePrice>
-          ) : undefined}
-        </Row>
-      </Wrapper>
-    </Link>
-  );
-};
+    <Flag style={{
+      '--bg-color': variant === 'on-sale' ? COLORS.primary : variant === 'new-release' ? COLORS.secondary : undefined
+    }}>{returnText(variant)}</Flag>
+ */
+	return (
+		<Link href={`/shoe/${slug}`}>
+			<Wrapper>
+				<ImageWrapper>
+					<Image alt='' src={imageSrc} />
+          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+          {variant === 'new-release' && <NewFlag>New Released!</NewFlag>}
+				</ImageWrapper>
+				<Spacer size={12} />
+				<Row>
+					<Name>{name}</Name>
+					<Price
+						style={{
+							'--color': variant === 'on-sale' ? COLORS.gray[900] : undefined,
+							'--text-decoration': variant === 'on-sale' ? 'line-through' : undefined,
+						}}
+					>
+						{formatPrice(price)}
+					</Price>
+				</Row>
+				<Row>
+					<ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+					{variant === 'on-sale' ? <SalePrice>{formatPrice(salePrice)}</SalePrice> : undefined}
+				</Row>
+			</Wrapper>
+		</Link>
+	)
+}
 
 const Link = styled.a`
-  text-decoration: none;
-  color: inherit;
-`;
+	text-decoration: none;
+	color: inherit;
+`
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article``
 
 const ImageWrapper = styled.div`
-  position: relative;
-`;
+	position: relative;
+`
 
 const Flag = styled.span`
-  position: absolute;
-  right: -4px;
-  top: 12px;
-  border-radius: 2px;
-  padding: 0 10px;
-  line-height: 32px;
-  color: ${COLORS.white};
-  background-color: var(--bg-color);
-  font-size: ${ 14 / 18}rem;
-  font-weight: ${WEIGHTS.bold};
+	position: absolute;
+	right: -4px;
+	top: 12px;
+	border-radius: 2px;
+	padding: 0 10px;
+	line-height: 32px;
+	color: ${COLORS.white};
+	background-color: var(--bg-color);
+	font-size: ${14 / 18}rem;
+	font-weight: ${WEIGHTS.bold};
+`
+
+const SaleFlag = styled(Flag)`
+  background-color: ${COLORS.primary};
+`
+const NewFlag = styled(Flag)`
+  background-color: ${COLORS.secondary};
 `
 
 const Image = styled.img`
-  width: 100%;
-`;
+	width: 100%;
+`
 
 const Row = styled.div`
-  font-size: 1rem;
-  display: flex;
-  justify-content: space-between;
-`;
+	font-size: 1rem;
+	display: flex;
+	justify-content: space-between;
+`
 
 const Name = styled.h3`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.gray[900]};
-`;
+	font-weight: ${WEIGHTS.medium};
+	color: ${COLORS.gray[900]};
+`
 
 const Price = styled.span`
-  color: var(--color);
-  text-decoration: var(--text-decoration)
-`;
+	color: var(--color);
+	text-decoration: var(--text-decoration);
+`
 
 const ColorInfo = styled.p`
-  color: ${COLORS.gray[700]};
-`;
+	color: ${COLORS.gray[700]};
+`
 
 const SalePrice = styled.span`
-  font-weight: ${WEIGHTS.medium};
-  color: ${COLORS.primary};
-`;
+	font-weight: ${WEIGHTS.medium};
+	color: ${COLORS.primary};
+`
 
-export default ShoeCard;
+export default ShoeCard
